41c4aa2d6dde4005b81865faaa3c50b8576e88db
andresmauro17 <andresmauro17@gmail.com>
2020-04-08 13:27:41 -0500

    chaged gulp, package.json, readme

--- src/Front/README.md
+++ src/Front/README.md
@@ -1,33 +1,26 @@
-# First of all, we thank you for your purchase and willingness of using Front in your projects! #
+# Welcome to Front – Multipurpose Responsive Template! #
 
-With an incredibly beautiful, fully responsive, and mobile-first projects on the web – Front Multipurpose Responsive Template is the perfect starting point for any creative and professional sites. Get started with Front’s components and options for laying out your Front project, including SVG components, powerful scripts, fully detailed documentation, and yet developer friendly code.
+Front Template by Htmlstream
 
+### Documentation and Getting Started ###
 
-### How to start with Front? ###
+The overall development documentation is available at `documentation/index.html` and Gulp documentation at `documentation/getting-started/gulp.html`.
 
-It's simple and easy, just open `documentation/index.html` and Front's documentation will guide you with detailed step by step information.
+Below is quick steps to run Gulp:
 
+- npm install
+- gulp
 
-### Where to download the Sketch version of Front? ###
-
-In order to download full sketch version, please open the below link and use the provided password:
-
-Download Link: http://bit.ly/front-template-skecht
-Password: 9tpfrontW[jXpskecht(8
-
-The reason why we include Sketch file separately is to reduce the original download package size after purchase.
-
+Yup, that's it.
 
 ### License ###
 
 Front is licensed under Bootstrap Themes and you can find more detailed information about it here: https://themes.getbootstrap.com/licenses
 
+### Theme Support ###
 
-### Free updates and support ###
-
-We would like to draw your attention to the fact that after purchasing a Front Template copy, you get the right for a lifetime entitlement to download updates for FREE! Need help? For any questions or concerns, reach us out at support@htmlstream.com
-
+Have a question? No worries! Front comes with 6 months of free support. We take seriously every issue that is reported to us, and we aim to resolve each one as quickly as possible. Feel free to “Contact Us” at https://htmlstream.com/contact-us
 
 ### Need Front Template customization? ###
 
-We offer affordable, professional and trendy customized design solutions, solely for your own projects! Front Template can be easily customized with its cutting-edge components and features. However, if you feel you need any further customization please drop us a message at clients@htmlstream.com and our dedicated team will assist you with your inquiries.
+We offer affordable, professional and trendy customized design solutions, solely for your own projects! Front Template can be easily customized with its cutting-edge components and features. However, if you feel you need any further customization please drop us a message at https://htmlstream.com/hire-us and our dedicated team will assist you with your inquiries.
--- src/Front/gulpfile.js
+++ src/Front/gulpfile.js
@@ -1,26 +1,27 @@
 //
 // Gulpfile
 //
-var gulp                   = require('gulp'),
-    sass                   = require('gulp-sass'),
-    changed                = require('gulp-changed'),
-    autoprefixer           = require('gulp-autoprefixer'),
-    rename                 = require('gulp-rename'),
-    del                    = require('del'),
-    concat                 = require('gulp-concat'),
-    cssnano                = require('gulp-cssnano'),
-    uglify                 = require('gulp-uglifyjs'),
-    cache                  = require('gulp-cache'),
-    imagemin               = require('gulp-imagemin'),
-    imageminJpegRecompress = require('imagemin-jpeg-recompress'),
-    pngquant               = require('imagemin-pngquant'),
-    browserSync            = require('browser-sync').create();
+
+"use strict";
+
+const gulp                   = require('gulp');
+const sass                   = require('gulp-sass');
+const changed                = require('gulp-changed');
+const autoprefixer           = require('gulp-autoprefixer');
+const rename                 = require('gulp-rename');
+const del                    = require('del');
+const concat                 = require('gulp-concat');
+const cleanCSS               = require('gulp-clean-css');
+const uglify                 = require('gulp-uglify-es').default;
+const cache                  = require('gulp-cache');
+const browsersync            = require('browser-sync').create();
 
 
 
 //
 // Gulp plumber error handler - displays if any error occurs during the process on your command
 //
+
 function errorLog(error) {
   console.error.bind(error);
   this.emit('end');
@@ -31,37 +32,38 @@ function errorLog(error) {
 //
 // SASS - Compile SASS files into CSS
 //
-gulp.task('sass', function () {
- // Theme
- gulp.src('./assets/scss/**/*.scss')
-  .pipe(changed('./assets/css/'))
-  .pipe(sass({ outputStyle: 'expanded' }))
-  .on('error', sass.logError)
-  .pipe(autoprefixer([
-      "last 1 major version",
-      ">= 1%",
-      "Chrome >= 45",
-      "Firefox >= 38",
-      "Edge >= 12",
-      "Explorer >= 10",
-      "iOS >= 9",
-      "Safari >= 9",
-      "Android >= 4.4",
-      "Opera >= 30"], { cascade: true }))
-  .pipe(gulp.dest('./assets/css/'))
-  .pipe(browserSync.stream());
-});
+
+function scss() {
+  return gulp
+    .src('./assets/scss/**/*.scss')
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest('./assets/css/'))
+    .on('error', sass.logError)
+    .pipe(autoprefixer([
+        "last 1 major version",
+        ">= 1%",
+        "Chrome >= 45",
+        "Firefox >= 38",
+        "Edge >= 12",
+        "Explorer >= 10",
+        "iOS >= 9",
+        "Safari >= 9",
+        "Android >= 4.4",
+        "Opera >= 30"], { cascade: true }))
+    .pipe(gulp.dest('./assets/css/'))
+    .pipe(browsersync.stream());
+}
 
 
 
 //
 // BrowserSync (live reload) - keeps multiple browsers & devices in sync when building websites
 //
-//
-gulp.task('serve', function() {
-  browserSync.init({
+
+function browserSync(done) {
+  browsersync.init({
     files: "./*.html",
-    startPath: "./html/home/",
+    startPath: "./html/landings/",
     server: {
       baseDir: "./",
       routes: {},
@@ -73,146 +75,121 @@ gulp.task('serve', function() {
         next();
       }
     }
-  })
-});
+  });
+  done();
+}
+
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
 
 
 
 //
 // Gulp Watch and Tasks
 //
-//
-gulp.task('watch', function() {
-  gulp.watch('./assets/scss/**/*.scss', ['sass']);
-  gulp.watch('./html/**/*.html').on('change', browserSync.reload);
-  gulp.watch('./starter/**/*.html').on('change', browserSync.reload);
-  gulp.watch('./documentation/**/*.html').on('change', browserSync.reload);
-});
+
+function watch() {
+  gulp.watch('./assets/scss/**/*.scss', scss);
+  gulp.watch(
+    [
+      './html/**/*.html',
+      './snippets/**/*.html',
+      './documentation/**/*.html'
+    ],
+    gulp.series(browserSyncReload)
+  );
+}
 
 // Gulp Tasks
-gulp.task('default', ['watch', 'sass', 'serve'])
+gulp.task('default', gulp.parallel(watch, scss, browserSync));
 
 
 
 //
-// CSS minifier - merges and minifies the below given list of Front libraries into one theme.min.css
+// CSS minifier - merges and minifies the below given list of Space libraries into one theme.min.css
 //
-gulp.task('minCSS', function() {
-  return gulp.src([
-    './assets/css/theme.css',
-  ])
-  .pipe(cssnano())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('./dist/assets/css/'));
-});
 
-
-
-//
-// JavaSript minifier - merges and minifies the below given list of Front libraries into one theme.min.js
-//
-gulp.task('minJS', function() {
-  return gulp.src([
-    './assets/js/hs.core.js',
-    './assets/js/components/hs.bg-video.js',
-    './assets/js/components/hs.chartist-area-chart.js',
-    './assets/js/components/hs.chartist-bar-chart.js',
-    './assets/js/components/hs.chart-pie.js',
-    './assets/js/components/hs.clipboard.js',
-    './assets/js/components/hs.countdown.js',
-    './assets/js/components/hs.counter.js',
-    './assets/js/components/hs.cubeportfolio.js',
-    './assets/js/components/hs.datatables.js',
-    './assets/js/components/hs.dropzone.js',
-    './assets/js/components/hs.fancybox.js',
-    './assets/js/components/hs.file-attach.js',
-    './assets/js/components/hs.focus-state.js',
-    './assets/js/components/hs.g-map.js',
-    './assets/js/components/hs.go-to.js',
-    './assets/js/components/hs.hamburgers.js',
-    './assets/js/components/hs.header.js',
-    './assets/js/components/hs.header-fullscreen.js',
-    './assets/js/components/hs.instagram.js',
-    './assets/js/components/hs.malihu-scrollbar.js',
-    './assets/js/components/hs.modal-window.js',
-    './assets/js/components/hs.onscroll-animation.js',
-    './assets/js/components/hs.password-strength.js',
-    './assets/js/components/hs.progress-bar.js',
-    './assets/js/components/hs.quantity-counter.js',
-    './assets/js/components/hs.range-datepicker.js',
-    './assets/js/components/hs.range-slider.js',
-    './assets/js/components/hs.scroll-effect.js',
-    './assets/js/components/hs.scroll-nav.js',
-    './assets/js/components/hs.selectpicker.js',
-    './assets/js/components/hs.show-animation.js',
-    './assets/js/components/hs.slick-carousel.js',
-    './assets/js/components/hs.step-form.js',
-    './assets/js/components/hs.sticky-block.js',
-    './assets/js/components/hs.summernote-editor.js',
-    './assets/js/components/hs.svg-injector.js',
-    './assets/js/components/hs.toggle-state.js',
-    './assets/js/components/hs.unfold.js',
-    './assets/js/components/hs.validation.js',
-    './assets/js/components/hs.video-player.js',
-    './assets/js/theme-custom.js',
-  ])
-  .pipe(concat('theme.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('./dist/assets/js/'));
-});
+function minCSS() {
+  return gulp
+    .src([
+      './assets/css/theme.css',
+    ])
+    .pipe(cleanCSS({compatibility: 'ie11'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist/assets/css/'));
+}
 
 
 
 //
-// Image minifier - compresses images
+// JavaSript minifier - merges and minifies the below given list of Space libraries into one theme.min.js
 //
-gulp.task('minIMG', function() {
-  return gulp.src('./assets/img/**/*')
-    .pipe(cache(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.jpegtran({progressive: true}),
-      imageminJpegRecompress({
-        loops: 5,
-        min: 65,
-        max: 70,
-        quality:'medium'
-      }),
-      imagemin.svgo(),
-      imagemin.optipng({optimizationLevel: 3}),
-      pngquant({quality: '65-70', speed: 5})
-    ],{
-      verbose: true
-    })))
-    .pipe(gulp.dest('./dist/assets/img/'));
-});
+
+function minJS() {
+  return gulp
+    .src([
+      './assets/js/hs.core.js',
+      './assets/js/hs.chartjs.js',
+      './assets/js/hs.circles.js',
+      './assets/js/hs.clipboard.js',
+      './assets/js/hs.countdown.js',
+      './assets/js/hs.cubeportfolio.js',
+      './assets/js/hs.datatables.js',
+      './assets/js/hs.dropzone.js',
+      './assets/js/hs.fancybox.js',
+      './assets/js/hs.flatpickr.js',
+      './assets/js/hs.ion-range-slider.js',
+      './assets/js/hs.leaflet.js',
+      './assets/js/hs.pwstrength.js',
+      './assets/js/hs.selectpicker.js',
+      './assets/js/hs.slick-carousel.js',
+      './assets/js/hs.summernote-editor.js',
+      './assets/js/hs.tagify.js',
+      './assets/js/hs.validation.js',
+      './assets/js/theme-custom.js',
+    ])
+    .pipe(concat('theme.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/assets/js/'));
+}
 
 
 
 //
 // Copy Vendors - a utility to copy client-side dependencies into a folder
 //
-gulp.task('copyVendors', function() {
-  gulp.src([
-    './node_modules/*animate.css/**/*',
-    './node_modules/*bootstrap-select/**/*',
-    './node_modules/*chartist/**/*',
-    './node_modules/*custombox/**/*',
-    './node_modules/*clipboard/**/*',
-    './node_modules/*datatables/**/*',
-    './node_modules/*flag-icon-css/**/*',
-    './node_modules/*flatpickr/**/*',
-    './node_modules/*gmaps/**/*',
-    './node_modules/*instafeed.js/**/*',
-    './node_modules/*ion-rangeslider/**/*',
-    './node_modules/*jquery/**/*',
-    './node_modules/*jquery-migrate/**/*',
-    './node_modules/*jquery-validation/**/*',
-    './node_modules/*popper.js/**/*',
-    './node_modules/*summernote/**/*',
-    './node_modules/*svg-injector/**/*',
-    './node_modules/*typed.js/**/*',
-  ])
-  .pipe(gulp.dest('./dist/assets/vendor/'))
-});
 
-gulp.task('dist', ['copyVendors', 'minCSS', 'minJS', 'minIMG']);
+function copyVendors() {
+  return gulp
+    .src([
+      './node_modules/*aos/**/*',
+      './node_modules/*bootstrap/**/*',
+      './node_modules/*bootstrap-select/**/*',
+      './node_modules/*chart.js/**/*',
+      './node_modules/*clipboard/**/*',
+      './node_modules/*datatables/**/*',
+      './node_modules/*dropzone/**/*',
+      './node_modules/*flag-icon-css/**/*',
+      './node_modules/*flatpickr/**/*',
+      './node_modules/*ion-rangeslider/**/*',
+      './node_modules/*jquery/**/*',
+      './node_modules/*jquery-countdown/**/*',
+      './node_modules/*jquery-migrate/**/*',
+      './node_modules/*jquery-validation/**/*',
+      './node_modules/*leaflet/**/*',
+      './node_modules/*pwstrength-bootstrap/**/*',
+      './node_modules/*summernote/**/*',
+      './node_modules/*tagify/**/*',
+      './node_modules/*typed.js/**/*',
+    ])
+    .pipe(gulp.dest('./dist/assets/vendor/'))
+};
+
+
+
+gulp.task('minCSS', minCSS);
+gulp.task('minJS', minJS);
+gulp.task('copyVendors', copyVendors);
+gulp.task('dist', gulp.series(copyVendors, minCSS, minJS));
