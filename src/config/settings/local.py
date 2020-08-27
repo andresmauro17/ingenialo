49e75d041d2c2a630666dbf6f474408be86c88d0
andresmauro17 <andresmauro17@gmail.com>
2020-08-27 16:48:27 -0500

    add domains

--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.getenv('INGENIALO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','157.245.245.146','ingenialo.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','157.245.245.146','ingenialo.com','www.ingenialo.com','ecdb.ingenialo.com','www.ingenialo.io','ingenialo.io']
 
 INSTALLED_APPS += (
     'debug_toolbar',
