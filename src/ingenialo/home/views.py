eac8fb80abc9451b4a9e2d322f93b9243434c98f
andresmauro17 <andresmauro17@gmail.com>
2020-10-10 09:50:27 -0500

    remove hero from the view render

--- src/ingenialo/home/views.py
+++ src/ingenialo/home/views.py
@@ -3,11 +3,9 @@ from .models import Hero
 from django.conf import settings
 
 def home_page(request):
-    heros = Hero.objects.filter(is_active=True).order_by('-position')
     context = {
         "title":"Ingenialo Industrias, explora tu instinto MAKER!",
         "content":"welcome to home page",
-        'heros':heros,
     }
     return render(request, "home/home_page.html", context)
 
