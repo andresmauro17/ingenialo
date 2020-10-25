3b39603a844d79597a1dfc971040a2f5e9d60826
andresmauro17 <andresmauro17@gmail.com>
2020-10-25 13:14:17 -0500

    refactoring csrftoken

--- src/ingenialo-ui/src/api/index.js
+++ src/ingenialo-ui/src/api/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
+import cookiesService from '@/services/cookiesService.js'
 
 var urlIn = window.location.href
 var arr = urlIn.split("/");
 var currentUrl = arr[0] + "//" + arr[2]+"/api"
-import cookiesService from '@/services/cookiesService.js'
+var csrftoken = cookiesService.getCookieByName('csrftoken') // django csrftoken
 
 let instance = axios.create({
     baseURL: currentUrl,
-    headers: {'X-CSRFToken': cookiesService.getCookieByName('csrftoken')},
+    headers: {'X-CSRFToken': csrftoken},
     //send a pseudo params for the browser doesnt take past cache when back
     params: {
         pseudoParam: new Date().getTime()
